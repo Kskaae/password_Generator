59a4957fbf14d654fad73b993880c4f16b4e8dd8
Kristopher Kaae <kskaae@gmail.com>
2020-08-29 15:07:39 -0700

    fixed js, it works

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Constants
+
 const generateBtn = document.querySelector("#generate");
 
 // Functions
@@ -20,8 +20,8 @@ function generatePassword(values){
 
   // Password is currently blank! We need to make a better one
   let lowerCase=confirm("Do you want to use lowercase letters");
-  let  uppercase=confirm("Do you want to use uppercase letters?");
-  let  numbers=confirm("Do you want to use numbers?");
+  let upperCase=confirm("Do you want to use uppercase letters?");
+  let numbers=confirm("Do you want to use numbers?");
   let special=confirm("Do you want to use special characters?")
   let length=prompt("Choose a password between 1 and 128")
 
@@ -30,38 +30,39 @@ function generatePassword(values){
   let upperCaseLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let numCharr="1234567890"
   let specialCharacters="@!#$%^&*"
-  // or
+  
   let valCharr=""
-  Console.log ("password","confirm", "lowercase", "uppercase", "numbers", "special characters", "prompt");
 
-  // Set password length/complexity//
-  let complexity=document.getElementById("#password") .values;
+  if (lowerCase) {
+    valCharr = valCharr + lowerCaseLetters
+
+   }
+
+   if (upperCase) {
+     valCharr = valCharr + upperCaseLetters
+   }
+
+   if (numbers) {
+    valCharr = valCharr + numCharr
+   }
+
+   if (special) {
+     valCharr = valCharr + specialCharacters
+   }
+
+  // Set password length
+
   // password values
-  let password =("")
+  let password =""
 
-  //For Loop
-  for (let i=0; i <= complexity; i++){
-    password= password+values.charAt(math.floor() * math.floor(values.length-1));
+  // For Loop
+  for (let i=0; i <= length; i++){
+    password = password+valCharr.charAt(Math.floor( Math.random()* valCharr.length));
+
   }
-
-  // add password to text box/display
-  document.getElementById("display").value= password;
-
-  if(document.getElementById("#password").value > 0){
-    document.getElementById("length").innerHTML="length: " + document.getElementById("#password") .value; 
-  }
- else{
-   document.getElementById("length") .innerHTML= ""
- }
- function UserInput(ps) {
-  document.getElementById("password").textContent = ps;
-
-
 
   return password;
 }
-
-// Main Process
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
